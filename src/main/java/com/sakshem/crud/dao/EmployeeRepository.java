ff09a2073b5f5440769fc5dc6a03b30323ff70a7
Sakshem Jain <sakshemjain7960@gmail.com>
2022-02-06 22:26:27 +0530

    Update EmployeeRepository.java

--- src/main/java/com/sakshem/crud/dao/EmployeeRepository.java
+++ src/main/java/com/sakshem/crud/dao/EmployeeRepository.java
@@ -6,5 +6,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 // This annotation will replace /--/employee path to /--/members
 //@RepositoryRestResource(path="members")
 public interface EmployeeRepository extends JpaRepository<Employee, Integer> {
-    // that's it ... no need to write any code.
+    
 }
